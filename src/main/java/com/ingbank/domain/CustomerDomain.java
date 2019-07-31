353224d7cddd4e9c215ae2cfe5cd10204098a6a6
ingspain0013 <ingspain0013@ingspain0013.eu1ddtfl0nnuvo1ibnn5qyespe.sx.internal.cloudapp.net>
2019-07-31 10:44:08 +0000

    Updated the userId.

--- src/main/java/com/ingbank/domain/CustomerDomain.java
+++ src/main/java/com/ingbank/domain/CustomerDomain.java
@@ -12,68 +12,72 @@ import javax.persistence.Table;
 @Table(name = "TBL_CUSTOMER")
 public class CustomerDomain {
 
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "UUID")
 	private Long uuid;
-	
-	@Column(name = "ID")
-	private String Id;
-	
+
+	@Column(name = "USERID")
+	private String userId;
+
 	@Column(name = "FIRSTNAME")
 	private String firstName;
-	
-	
+
 	@Column(name = "LASTNAME")
 	private String lastName;
-	
+
 	@Column(name = "ADDRESS")
-	private String address ;
-	
+	private String address;
+
 	@Column(name = "EMAILID")
 	private String emailId;
-	
+
 	public Long getUuid() {
 		return uuid;
 	}
+
 	public void setUuid(Long uuid) {
 		this.uuid = uuid;
 	}
-	public String getId() {
-		return Id;
+
+	public String getUserId() {
+		return userId;
 	}
-	public void setId(String id) {
-		Id = id;
+
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
-	
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-	
-	
-	
-	
+
 }
